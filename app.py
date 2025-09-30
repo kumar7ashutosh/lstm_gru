dd2100917af1b0b6db34bb83c54cb81d3e1d7761
kumar7ashutosh <151048515+kumar7ashutosh@users.noreply.github.com>
2025-10-01 02:05:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model=load_model('next_word.h5')
+with open('tokenizer.pkl') as f:
+  tokenizer=pickle.load(f)
+
+def predict_next_word(model,tokenizer,text,max_sequence_len):
+  token_list=tokenizer.texts_to_sequences([text])[0]
+  if len(token_list)>=max_sequence_len:
+    token_list=token_list[-(max_sequence_len-1):]
+  token_list=pad_sequences([token_list],maxlen=max_sequence_len-1,padding='pre')
+  predicted=model.predict(token_list,verbose=0)
+  predicted_word_index=np.argmax(predicted,axis=1)
+  for word,index in tokenizer.word_index.items():
+    if index==predicted_word_index:
+      return word
+  return None
+
+st.title('Next word prediction using lstm and gru')
+ip_text=st.text_input('enter text')
+if st.button('predict'):
+  max_sequence_len=model.input_shape[i]+1
+  next_word=predict_next_word(model,tokenizer,ip_text,max_sequence_len)
+  st.write(f' next word : {next_word}')
